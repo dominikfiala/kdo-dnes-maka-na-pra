0b374d2b6bf609cd84476508653ad9de21a7fe49
Dominik Fiala <domo.fiala@gmail.com>
2022-02-08 14:22:55 +0100

    feat: alternativní zápis datumu a podchycení chyb

--- api/pra.js
+++ api/pra.js
@@ -3,35 +3,19 @@ const router = express.Router();
 const {google} = require("googleapis");
 const {startOfWeek, endOfWeek, format} = require("date-fns");
 const {cs} = require("date-fns/locale");
+const package = require('../package.json');
 
-const week = () => {
+const rangeSuggestions = () => {
     const weekStart = startOfWeek(new Date, {weekStartsOn: 1});
     const weekEnd = endOfWeek(new Date, {weekStartsOn: 1});
-    const week = format(weekStart, 'd.M.Y') + '-' + format(weekEnd, 'd.M.Y');
-    return week;
+    return [
+        format(weekStart, 'd.M.') + '-' + format(weekEnd, 'd.M.Y'),
+        format(weekStart, 'd.M.Y') + '-' + format(weekEnd, 'd.M.'),
+        format(weekStart, 'd.M.Y') + '-' + format(weekEnd, 'd.M.Y'),
+    ]
 };
 
-/**
- * GET product list.
- *
- * @return product list | empty.
- */
-router.get("/", async (req, res) => {
-    const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-    const {privateKey} = JSON.parse(process.env.GOOGLE_SHEETS_PRIVATE_KEY);
-    const jwt = new google.auth.JWT(
-        process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        null,
-        privateKey,
-        target
-    );
-
-    const googleSheetsInstance = google.sheets({version: "v4", auth: jwt});
-    const response = await googleSheetsInstance.spreadsheets.values.get({
-        spreadsheetId: '11myQ8aDwNwWX3d-uRjqVVZkektY9D96jMkAJgSA4ErU',
-        range: week(), // sheet name
-    });
-
+const prepareTplVars = (response) => {
     const data = response.data.values;
     const amColIndex = (new Date).getDay() * 2;
     const pmColIndex = amColIndex + 1;
@@ -50,63 +34,105 @@ router.get("/", async (req, res) => {
     });
 
     const unknownName = 'To se neví';
-    const vars = {
+    return {
         amName: data[amRowIndex] ? data[amRowIndex][1] : unknownName,
         pmName: data[pmRowIndex] ? data[pmRowIndex][1] : unknownName,
         title: 'Kdo dnes maká na Pra?',
         date: format(new Date, 'EEEE d. MMMM Y', {locale: cs})
     }
-    const template = `<!DOCTYPE html>
-        <html lang="cs">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>${vars.title}</title>
-            <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css">
-            <style>
-                body {
-                    padding: 1rem;
-                    text-align: center;
-                }
-            </style>
-            <script>
-                self._ga = {
-                    // Full Measurement Protocol param reference:
-                    // https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
-                    data: {
-                        v: "1", // Measurement Protocol version.
-                        tid: "UA-217210080-1", // Tracking ID.
-                        cid: \`${Date.now()}${Math.random()}\`, // Client ID.
-                        dl: location.href, // Document location.
-                        aip: 1, // Anonymize IP
-                        dr: document.referrer,
-                    },
-                    send(additionalParams) {
-                        navigator.sendBeacon(
-                            "https://google-analytics.com/collect",
-                            new URLSearchParams({
-                                ...this.data,
-                                ...additionalParams,
-                            }).toString()
-                        );
-                    },
-                };
-                _ga.send({t: "pageview"});
-            </script>
-        </head>
-        <body>
-        <h1>${vars.title}</h1>
-        <h2>${vars.date}</h2>
-        <p>Odpoledne:</p>
-        <h3>${vars.amName}</h3>
-        <p>Večer:</p>
-        <h3>${vars.pmName}</h3>
-        </body>
-        </html>`;
+};
+
+const template = (vars) => `<!DOCTYPE html>
+    <html lang="cs">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>${vars.title}</title>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css">
+        <style>
+            body {
+                padding: 1rem;
+                text-align: center;
+            }
+        </style>
+        <script>
+            self._ga = {
+                // Full Measurement Protocol param reference:
+                // https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+                data: {
+                    v: "1", // Measurement Protocol version.
+                    tid: "UA-217210080-1", // Tracking ID.
+                    cid: \`${Date.now()}${Math.random()}\`, // Client ID.
+                    dl: location.href, // Document location.
+                    aip: 1, // Anonymize IP
+                    dr: document.referrer,
+                },
+                send(additionalParams) {
+                    navigator.sendBeacon(
+                        "https://google-analytics.com/collect",
+                        new URLSearchParams({
+                            ...this.data,
+                            ...additionalParams,
+                        }).toString()
+                    );
+                },
+            };
+            _ga.send({t: "pageview"});
+        </script>
+    </head>
+    <body>
+    <h1>${vars.title}</h1>
+    ${vars.body}
+    </body>
+    </html>`;
+
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
+router.get("/", async (req, res) => {
+    const target = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+    const {privateKey} = JSON.parse(process.env.GOOGLE_SHEETS_PRIVATE_KEY);
+    const jwt = new google.auth.JWT(
+        process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        null,
+        privateKey,
+        target
+    );
+
+    const googleSheetsInstance = google.sheets({version: "v4", auth: jwt});
+
+    let response;
+    for (const rangeSuggestion of rangeSuggestions()) {
+        try {
+            response = await googleSheetsInstance.spreadsheets.values.get({
+                spreadsheetId: '11myQ8aDwNwWX3d-uRjqVVZkektY9D96jMkAJgSA4ErU',
+                range: rangeSuggestion, // sheet name
+            })
+        } catch (e) {
+        }
+    }
+
+    let body = '<h2>Chyba získávání dat 🥺</h2>';
+    if (response) {
+        let vars = prepareTplVars(response);
+        body = `<h2>${vars.date}</h2>
+            <p>Odpoledne:</p>
+            <h3>${vars.amName}</h3>
+            <p>Večer:</p>
+            <h3>${vars.pmName}</h3>`;
+    }
+
+    const data = {
+        title: package.description,
+        body: body,
+    };
+    const payload = template(data);
 
     try {
-        res.status(200).send(template);
+        res.status(200).send(payload);
     } catch (error) {
         console.error(error);
         return res.status(500).send("Server error");
